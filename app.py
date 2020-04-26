872e61dc70d6532f81b590b112c237ffdfdd9360
Johandre van Deventer <johandrevdeventer@gmail.com>
2020-04-26 22:24:51 +0200

    added deploy notification

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from flask import Flask
 from time import sleep
 
 from pubnub_control import PubNubControlClass
+from mail_sender import MailSenderCls
 
 pubnub_control_class = PubNubControlClass()
+mail_sender_cls = MailSenderCls()
 
 app = Flask(__name__)
 
@@ -17,10 +19,8 @@ def index():
     return 'Hello'
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":  
 
-    while True:
-        print('Testing')
-        sleep(1)
+    mail_sender_cls.send_mail('Application', 'Deployed')  
 
     app.run(host='0.0.0.0', threaded=True, use_reloader=False)
