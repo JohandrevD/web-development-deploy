503a0d66a234342de85522df31db1eaeaf4f6301
Johandre van Deventer <johandrevdeventer@gmail.com>
2020-04-26 22:15:45 +0200

    added raspberry pi monitoring

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ from flask import Flask
 from time import sleep
 
 from pubnub_control import PubNubControlClass
-# from mail_sender import MailSenderCls
-
-pubnub_control_class = PubNubControlClass()
-# mail_sender_cls = MailSenderCls()
 
 app = Flask(__name__)
 
@@ -16,12 +12,12 @@ app = Flask(__name__)
 def index():
     """ Default route page  """
 
-    # mail_sender_cls.send_mail('Testing', 'Testing')
-
     return 'Hello'
 
 
 if __name__ == "__main__":
+
+    pubnub_control_class = PubNubControlClass()
 
     while True:
         print('Testing')
