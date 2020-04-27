e8b011c735e8f4655ea43b74a4303867e8b0f601
Johandre van Deventer <johandrevdeventer@gmail.com>
2020-04-27 17:29:46 +0200

    iot page access prompt

--- static/javascript/iot.js
+++ static/javascript/iot.js
@@ -1,3 +1,40 @@
+var t_day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var t_month=["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+var lamp_status = true;
+var lamp_button = document.getElementById('lamp_icon');
+
+//  Function to get and display the server status
+function GetServerStatus()
+{
+    //Setup server status server sent event endpoint
+    var ServerStatusSource = new EventSource('/server_status/');
+
+    ServerStatusSource.onmessage = function(e)
+    {
+        document.getElementById('server_status_value').innerHTML = e.data;
+    }
+}
+
+//  Function to get and display the current time and date
+function GetClock()
+{
+    var d = new Date();
+    var n_day = d.getDay(), n_month = d.getMonth(), n_date = d.getDate(), n_year = d.getFullYear();
+    var n_hour = d.getHours(), n_min = d.getMinutes(), n_sec = d.getSeconds();
+
+    if(n_min<=9) n_min = "0" + n_min;
+    if(n_sec<=9) n_sec = "0" + n_sec;
+
+    var time_text = "" + n_hour + ":" + n_min + ":" + n_sec + "";
+    var date_text = "" + t_day[n_day] + ", " + n_date + " " + t_month[n_month] + " " + n_year;
+    document.getElementById('time_display').innerHTML = time_text;
+    document.getElementById('date_display').innerHTML = date_text;
+}
+
+GetClock();
+setInterval(GetClock,1000);
+
 // ----- PubNub control starts here ----- //
 const clientUUID = 'Website_IoT_HTML';
 const theChannel = 'Web_Control';
