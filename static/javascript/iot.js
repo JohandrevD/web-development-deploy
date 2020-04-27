1b3969dfade41746d74a9ea81a3464fa6056f996
Johandre van Deventer <johandrevdeventer@gmail.com>
2020-04-27 13:52:10 +0200

    iot page connection control

--- static/javascript/iot.js
+++ static/javascript/iot.js
@@ -29,8 +29,8 @@ function check_pi(){
                 window.location.href = window.location.href.split('/')[0] + 'iot';
             }
             else{
-                alert('Controller is not running');
-                window.location.href = window.location.href.split('/')[0] + 'index';
+                // alert('Controller is not running');
+                // window.location.href = window.location.href.split('/')[0] + 'index';
                 console.log(window.location.href);
                 // document.querySelector('h5').innerHTML = 'Controller is not running';
                 // document.getElementById('enter-btn').innerHTML = 'No access';
