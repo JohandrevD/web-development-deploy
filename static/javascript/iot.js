09bf363099ad16b71b29eed84f653c225897e71a
Johandre van Deventer <johandrevdeventer@gmail.com>
2020-04-27 13:54:47 +0200

    iot page connection control

--- static/javascript/iot.js
+++ static/javascript/iot.js
@@ -25,16 +25,17 @@ function check_pi(){
             for(let val in connected_channel){
                 users.push(connected_channel[val].uuid)
             }
-            if(users.includes('Raspberry_Pi')){
-                window.location.href = window.location.href.split('/')[0] + 'iot';
-            }
-            else{
-                // alert('Controller is not running');
-                // window.location.href = window.location.href.split('/')[0] + 'index';
-                console.log(window.location.href);
-                // document.querySelector('h5').innerHTML = 'Controller is not running';
-                // document.getElementById('enter-btn').innerHTML = 'No access';
-            }              
+            console.log(window.location.href);
+            // if(users.includes('Raspberry_Pi')){
+            //     window.location.href = window.location.href.split('/')[0] + 'iot';
+            // }
+            // else{
+            //     // alert('Controller is not running');
+            //     // window.location.href = window.location.href.split('/')[0] + 'index';
+            //     console.log(window.location.href);
+            //     // document.querySelector('h5').innerHTML = 'Controller is not running';
+            //     // document.getElementById('enter-btn').innerHTML = 'No access';
+            // }              
         }
     );
 };
